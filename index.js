d19968709727e0c5804bc1a3b099085496596944
maheshv09 <vaswani.mahesh2012@gmail.com>
2024-03-01 12:28:38 +0530

    Initialized server

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+  try {
+    app.get("/", (req, res) => {
+      res.send("Hello from Sever!!");
+    });
+
+    app.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  } catch (error) {}
+}
+run();
+
+module.exports = app;
