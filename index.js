c7e74cf35a176a2f174f700ff94ae36ec5d993ed
maheshv09 <vaswani.mahesh2012@gmail.com>
2024-03-01 14:54:40 +0530

    Added fetch api

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const { createClient } = require("@supabase/supabase-js");
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +18,17 @@ async function run() {
   try {
     app.get("/", (req, res) => {
       res.send("Hello from Sever!!");
+    });
+
+    app.get("/fetchData", async (req, res) => {
+      const { data, error } = await supabase.from("user").select();
+      //console.log("DATAAA", data);
+      if (error) {
+        return res
+          .status(500)
+          .json({ error: "Failed to fetch data from Supabase" });
+      }
+      res.json(data);
     });
 
     app.listen(port, () => {
